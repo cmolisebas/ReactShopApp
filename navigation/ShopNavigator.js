a4df44d43241bcec5a6b06381c744f6c578cb0e9
Sebastian Molina Castaño <smolcastano@gmail.com>
2019-12-17 17:52:23 -0500

    se adicionan pantallas
    se adicionan pantallas

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,14 +1,25 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { Platform } from 'react-native';
 
 import ProductOverViewScreen from '../screens/shop/ProductOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator({
-    ProductsOverview: ProductOverViewScreen
+    ProductsOverview: ProductOverViewScreen,
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen
 }, {
     defaultNavigationOptions: {
         headerStyle: {
+            headerTitleStyle:{
+                fontFamily: 'open-sans-bold'
+            },
+            headerBackTitleStyle:{
+                fontFamily: 'open-sans'
+            },
             backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
         },
         headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
